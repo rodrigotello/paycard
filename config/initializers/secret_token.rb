67bbf8afdf9c8d7dc39456780b32c843a7689cbf
rodrigotello <rodrigotello@outlook.com>
2013-07-04 14:09:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paycard::Application.config.secret_token = '675a43fa96ce351b62b2b2572caf854419081a73d7c44c6427c764c2f4e06fdf5883e82fc9b86c3f1652db1adb360bfe1e863ee9c1dfab869defa4166c0a9357'
