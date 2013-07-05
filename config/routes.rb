867ffb97ff406479f64a7e3b388f60fe0677d267
rodrigotello <rodrigotello@outlook.com>
2013-07-04 19:13:22 -0500

    Finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Paycard::Application.routes.draw do
+  get "users/new"
+
+ root :to => 'static_pages#home'
+
+  match 'signup', to: 'users#new'
+  match "/help", to: "static_pages#help"
+
+  match "/about", to: "static_pages#about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
